c51952413dccce7c4649ba7155a35bd1751ebc45
Fran Swan <fran@boringprotocol.io>
2023-07-29 20:53:37 -0700

    Create convert_markdown_to_json.js

---
+++ scripts/convert_markdown_to_json.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function markdownToJSON(bookName, markdownContent) {
+  const lines = markdownContent.split(/\r?\n/);
+  let chapters = [];
+  let currentChapter = null;
+  let currentVerseNum = null;
+
+  for (const line of lines) {
+    const chapterMatch = line.match(/^## Chapter (\d+)$/);
+
+    if (chapterMatch) {
+      if (currentChapter) {
+        chapters.push({ ...currentChapter, num: parseInt(chapterMatch[1]) });
+      }
+      currentChapter = { verses: [] };
+      currentVerseNum = null;
+    } else {
+      const verseMatch = line.match(/^(\d+)\. (.+)$/);
+      if (verseMatch) {
+        currentVerseNum = parseInt(verseMatch[1]);
+        currentChapter.verses.push({ text: verseMatch[2].trim(), num: currentVerseNum });
+      } else if (currentChapter && line.trim() !== "") {
+        if (currentVerseNum !== null) {
+          currentChapter.verses.push({ text: line.trim() });
+        } else {
+          // If there are lines outside verses, add them as verses without numbers
+          currentChapter.verses.push({ text: line.trim() });
+        }
+      }
+    }
+  }
+
+  if (currentChapter) {
+    chapters.push({ ...currentChapter, num: null });
+  }
+
+  return { name: bookName, chapters };
+}
+
+const args = process.argv.slice(2);
+const bookName = args[0];
+const markdownContent = fs.readFileSync(path.join('books', 'markdown', `${bookName.toLowerCase()}.md`), 'utf8');
+const jsonContent = JSON.stringify(markdownToJSON(bookName, markdownContent), null, 2);
+fs.writeFileSync(path.join('books', 'json', `${bookName.toLowerCase()}.json`), jsonContent, 'utf8');
